e045458097c5445fdeea0df692fef5db2244d21f
natanbrito <natanbrito.mtm@outlook.com>
2020-05-24 15:23:08 -0300

    Initial Commit

---
+++ SwiftNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftNotes
+//
+//  Created by Natan Brito on 24/05/20.
+//  Copyright © 2020 Natan Brito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
