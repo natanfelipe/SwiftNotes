1e6af53f809c8b9b4031eaef70c9a4e1cf4eeae6
natanbrito <natanbrito.mtm@outlook.com>
2020-05-24 15:44:25 -0300

    Creation of layout and view controllers

---
+++ SwiftNotes/NoteListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  NoteListViewController.swift
+//  SwiftNotes
+//
+//  Created by Natan Brito on 24/05/20.
+//  Copyright © 2020 Natan Brito. All rights reserved.
+//
+
+import UIKit
+
+class NoteListViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        <#code#>
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+}
--- SwiftNotes/NoteViewController.swift
+++ SwiftNotes/NoteViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TaskViewController.swift
 //  SwiftNotes
 //
 //  Created by Natan Brito on 24/05/20.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class NoteViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
